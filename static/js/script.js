4ca2084f1edcd421680d544e00e7cc355b5e1cc8
Aylamccarthy <aylamarmol@yahoo.co.uk>
2024-02-25 20:36:10 +0000

    Add script for setting padding top of container to be equal with header height

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,27 @@
-// SCRIPT FOR NAVBAR SEARCH BOX TO BE DISPLAYED ONLY WHEN THE COLLAPSIBLE NAV IS OFF
-
-let navbarToggler = document.getElementById('navbarToggler');
-let navbarTogglerButton = document.getElementById('navbarTogglerButton');
-
-navbarTogglerButton.addEventListener('click', () => {
-    let searchElements = document.getElementsByClassName('search-bar');
-    if (!navbarToggler.classList.contains('show')){
-        for(let el of searchElements){
-            el.style.display = 'none';
-        }
-    }
-    else{
-        setTimeout(() => {
+document.addEventListener("DOMContentLoaded", function(event) { 
+    // SCRIPT FOR NAVBAR SEARCH BOX TO BE DISPLAYED ONLY WHEN THE COLLAPSIBLE NAV IS OFF
+    
+    let navbarToggler = document.getElementById('navbarToggler');
+    let navbarTogglerButton = document.getElementById('navbarTogglerButton');
+    
+    navbarTogglerButton.addEventListener('click', () => {
+        let searchElements = document.getElementsByClassName('search-bar');
+        if (!navbarToggler.classList.contains('show')){
             for(let el of searchElements){
-                el.style.display = 'flex';
+                el.style.display = 'none';
             }
-        }, 350);
-    }
+        }
+        else{
+            setTimeout(() => {
+                for(let el of searchElements){
+                    el.style.display = 'flex';
+                }
+            }, 350);
+        }
+    });
 });
+
+    // SCRIPT FOR SETTING PADDING TOP OF CONTENT CONTAINER TO BE EQUAL WITH HEADER HEIGHT
+    let headerHeight = document.getElementsByTagName('header')[0].offsetHeight;
+    let contentContainer = document.getElementsByClassName('content-container')[0];
+    contentContainer.style.paddingTop= (headerHeight - 2) + 'px';
