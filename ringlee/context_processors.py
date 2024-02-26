847835284024237d949b294f501c2b99c99f50b5
Aylamccarthy <aylamarmol@yahoo.co.uk>
2024-02-26 19:53:48 +0000

    Add newletter app urls to main app urls

---
+++ ringlee/context_processors.py
@@ -0,0 +1,12 @@
+"""
+ringlee - Context processors
+"""
+from newsletter.forms import AddSubscriber
+
+
+def add_subscription_form_to_context(request):
+    """Method to return subscription form as context"""
+
+    return {
+        'add_subscriber_form': AddSubscriber
+    }
